95439de713f7ce982464748af41cfca00c3ba80a
dylanbossie <dylanbossie@gmail.com>
2020-03-08 13:23:22 -0600

    Add background analog clock face

--- app/index.js
+++ app/index.js
@@ -21,6 +21,39 @@ const batteryLevel = document.getElementById("batteryLevel");
 const steps = document.getElementById("steps");
 var batteryIcon = document.getElementById("battery");
 
+let hourHand = document.getElementById("hours");
+let minHand = document.getElementById("mins");
+let secHand = document.getElementById("secs");
+
+// Returns an angle (0-360) for the current hour in the day, including minutes
+function hoursToAngle(hours, minutes) {
+  let hourAngle = (360 / 12) * hours;
+  let minAngle = (360 / 12 / 60) * minutes;
+  return hourAngle + minAngle;
+}
+
+// Returns an angle (0-360) for minutes
+function minutesToAngle(minutes) {
+  return (360 / 60) * minutes;
+}
+
+// Returns an angle (0-360) for seconds
+function secondsToAngle(seconds) {
+  return (360 / 60) * seconds;
+}
+
+// Rotate the hands every tick
+function updateClockHands() {
+  let today = new Date();
+  let hours = today.getHours() % 12;
+  let mins = today.getMinutes();
+  let secs = today.getSeconds();
+
+  hourHand.groupTransform.rotate.angle = hoursToAngle(hours, mins);
+  minHand.groupTransform.rotate.angle = minutesToAngle(mins);
+  secHand.groupTransform.rotate.angle = secondsToAngle(secs);
+}
+
 let dayOfWeekMap = new Map;
 dayOfWeekMap.set(0, "Sun");
 dayOfWeekMap.set(1, "Mon");
@@ -53,6 +86,8 @@ clock.ontick = (evt) => {
   // Format date information and display
   currentDate.text = `${month+1}/${day}`;
   currentDayOfWeek.text = `${dayOfWeekMap.get(dayOfWeek)}`;
+
+  updateClockHands();
 }
 
 // Update step count display
